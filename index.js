36aa54e2d51c6f1793f53185da9d605957c48f8e
jayreme <jayremesaramosing@gmail.com>
2024-09-06 11:32:52 +0800

    setting up database connection

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
-app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'))
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error'));
+db.once('open', () => {
+    console.log("Database Connected");
+})
+
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000");
+    console.log("Serving on port 3000");   
 })
